15e82ec5628b06f26badcf219e6ccfd0c387495c
Deng Xiaobin <124945666@qq.com>
2017-03-17 09:59:59 +0800

    Initial Commit

---
+++ FoldAndSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoldAndSearch
+//
+//  Created by JJT on 17/3/17.
+//  Copyright © 2017年 SJZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoldAndSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoldAndSearch
+//
+//  Created by JJT on 17/3/17.
+//  Copyright © 2017年 SJZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
